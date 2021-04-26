9968ca7cbdb17a6faf1ecb4649befb3151692979
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-04-26 18:38:02 -0400

    created models, server.js, connection.js, and auth.js utils

---
+++ models/index.js
@@ -0,0 +1,19 @@
+const User = require('./User');
+const BlogPost = require('./Blog-Post');
+const Comments = require('./Comments');
+
+User.hasMany(BlogPost, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+BlogPost.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+BlogPost.hasMany(Comments, {
+  foreignKey:'user_id',
+  onDelete: 'CASCADE'
+})
+
+module.exports = { User, BlogPost };
